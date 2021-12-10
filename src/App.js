8ff98b06d82c7ec9082f623900f681e9f31e0bf6
Zaki Amor <amorzaki05@gmail.com>
2021-12-10 15:52:37 +0100

    mycommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home'
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/login'  element={<Login/>} />
+      <Route path='/'  element={<Home/>}/>
+
+    </Routes>
+      
+   
   );
 }
 
