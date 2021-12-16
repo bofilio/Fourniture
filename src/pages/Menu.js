48b6bea0491007bc2783b6d6ece0a7f977e6182b
amorzaki <59356269+amorzaki@users.noreply.github.com>
2021-12-16 10:01:58 +0100

    Create Menu.js

---
+++ src/pages/Menu.js
@@ -0,0 +1,34 @@
+<div class=" w-298 h-200 bg-gray-200 rounded-tr-6xl rounded-br-6xl mr-48 flex space-x-12 overflow-y-auto pt-5">
+  <div class=" w-108 h-191 bg-gray-100 rounded-tr-6xl rounded-br-6xl flex flex-col space-y-28 pt-16 pl-16">
+    <div class="flex space-x-40">
+        <span class="text-4xl text-blx-900 font-medium">Menu</span>
+        <div class=" w-12 h-12 rounded-full bg-gray-300 flex justify-center items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </div>
+    </div>
+    
+    <div class="flex flex-col space-y-8">
+      <span class=" text-2xl text-gray-400">ALL</span>
+      <span class=" text-2xl text-gray-400">BESTSELLERS</span>
+      <span class=" text-2xl text-blx-900 line-through">SHOP COMMING SOON</span>
+      <span class=" text-2xl text-gray-400">GALLERY</span>
+
+    </div>
+    <div class="flex flex-col space-y-3">
+      <span class=" text-xl text-gray-400">About us</span>
+      <span class=" text-xl text-gray-400">News & Events</span>
+      <span class=" text-xl text-gray-400">Contact us</span>
+    </div>
+
+  </div>
+  <div class="flex flex-col  space-y-3 overflow-y-scroll px-4">
+    <div class="w-128 h-48 bg-gray-100 rounded-5xl flex-shrink-0 pt-10 pl-10 flex flex-col space-y-3">
+      <span class=" text-3xl text-blx-900 font-medium">Sofas & Daybeds</span>
+      <span class=" text-lg text-gray-400">1 --- 8</span>
+    </div>
+    <div class="w-128 h-48 bg-gray-100 rounded-5xl flex-shrink-0"></div>
+  </div>
+
+</div>
