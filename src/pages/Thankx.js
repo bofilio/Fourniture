ee2d0cfd1840b2416060e5c8c23920f8d5f60007
amorzaki <59356269+amorzaki@users.noreply.github.com>
2021-12-19 11:10:19 +0100

    Create Thankx.js

---
+++ src/pages/Thankx.js
@@ -0,0 +1,53 @@
+
+  <div class=" max-w-2xl w-full  max-h-64 h-auto flex flex-col items-center justify-center p-3">
+    <div class="w-32 h-32  rounded-full bg-blx-900 flex ">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-yellow-100 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+      </svg>
+    </div>
+    <div class=" w-128 h-11 flex  justify-center">
+      <span class=" text-4xl text-blx-900 font-medium">Your item in on the way</span>
+    </div>
+    <div class="w-136 h-16 flex items-center justify-center">
+      <span class=" text-xl text-gray-400 text-center">Vivamus rhoncus, orci accumsan rhoncus molestie, diam nulla consectetur lorem ulputate sem neque vitae sem.</span>
+    </div>
+
+  </div>
+
+
+  <div class="max-w-7xl w-full max-h-80 h-full bg-gray-100 flex  gap-4 py-14 pl-14 rounded-tl-5xl rounded-tr-5xl overflow-x-scroll">
+    <div class=" w-108 h-48 bg-white rounded-5xl flex-shrink-0 flex items-center pl-3 gap-6">
+      <div class=" w-32 h-32 bg-gray-50 rounded-3xl "></div>
+      <div class="flex flex-col">
+        <span class=" text-2xl text-blx-900">Catena Sofa Center</span>
+        <div class="flex gap-3">
+          <span class=" text-2xl text-gray-400 line-through">$1355,00</span>
+          <span class=" text-2xl text-orange-600">$955,00</span>
+        </div>
+        <div class="flex gap-5 w-full">
+          <div class="rounded-3xl bg-blc-200 py-3 px-8 flex justify-center items-center ">
+            <button class="flex space-x-3">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span class="text-white">Add to cart</span>
+            </button>
+          </div>
+          <div class=" w-14 h-14 bg-gray-50 rounded-3xl flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+      
+    </div>
+   
+
+  </div>
+
+
