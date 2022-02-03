718ac7dd8cbf763748fa16aed52ba71e2eadf1ee
amorzaki <59356269+amorzaki@users.noreply.github.com>
2022-02-03 10:49:51 +0100

    Create Filter.js

---
+++ src/pages/Filter.js
@@ -0,0 +1,61 @@
+<div class="flex flex-col max-w-sm h-auto max-h-7xl bg-primary rounded-4xl py-10">
+  <div class="flex space-x-32 mx-10">
+    <span class=" text-yellow-50 font-medium text-3xl">Filter by</span>
+    <div class="w-12 h-12 bg-gray-600 rounded-full flex justify-center items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-50" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </div>
+  </div>
+  <div class=" h-1.5 bg-gray-700 mx-7 mt-4"></div>
+  <div class="flex space-x-32 mx-10 mt-3">
+    <span class=" text-yellow-50 text-sm font-medium">PRICE RANGE</span>
+    <span class="text-yellow-200 text-sm font-medium">$200-$800</span>
+  </div>
+
+  <div class=" h-1.5 bg-gradient-to-r from-org to-gray-900 mx-7 mt-4 flex items-center px-6">
+    <div class=" w-7 h-7 bg-yellow-50 rounded-lg rotate-45"></div>
+  </div>
+  <div class="flex space-x-9 mx-16 mt-4">
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+    <div class="w-1 h-3 bg-gray-600"></div>
+  </div>
+  <div class=" h-1 bg-gray-700 mx-7 mt-4"></div>
+  <div class="flex space-x-32 mx-10 mt-3">
+    <span class=" text-yellow-50 text-sm font-medium">COLOR</span>
+    <span class="text-yellow-200 text-sm font-medium">BEIGE,MAGENTA</span>
+  </div>
+  <div class="flex mx-10 gap-4 mt-7 flex-wrap">
+    <div class=" w-9 h-9 rounded-full bg-yellow-50"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-700"></div>
+    <div class=" w-9 h-9 rounded-full bg-orange-500"></div>
+    <div class=" w-9 h-9 rounded-full bg-violet-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-blue-300"></div>
+    <div class=" w-9 h-9 rounded-full bg-green-300"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-600"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-gray-700"></div>
+  </div>
+  <div class=" h-1 bg-gray-700 mx-7 mt-4"></div>
+  <div class="flex gap-44 mx-10 mt-3">
+    <span class=" text-yellow-50 text-sm font-medium">MATERIAL</span>
+    <span class="text-yellow-200 text-sm font-medium">WOOD</span>
+  </div>
+  <div class="flex mx-10 gap-4 mt-7 flex-wrap">
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+    <div class=" w-9 h-9 rounded-full bg-yellow-800"></div>
+  </div>
+  <div class=" bg-gre mt-14 w-full max-h-16 flex items-center justify-center rounded-4xl">
+    <span class=" text-yellow-200 text-xl py-20 px-5 ">Save Changes</span>
+
+  </div>
+</div>
